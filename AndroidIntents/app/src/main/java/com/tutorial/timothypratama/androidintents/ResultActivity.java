92007464449fdf32241a4736e7ceaf141478207b
Timothy Pratama <timothy.pratama@gmail.com>
2015-02-07 11:58:53 +0700

    Added second activity

---
+++ AndroidIntents/app/src/main/java/com/tutorial/timothypratama/androidintents/ResultActivity.java
@@ -0,0 +1,13 @@
+package com.tutorial.timothypratama.androidintents;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
